c840bc1df73604d1e1681629beec1c4e65900a1b
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2024-09-07 11:04:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let x = 0;
+	for(let i=0;i<str.length;i++){
+		x^=str[i];
+	}
+
+	return x;
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
